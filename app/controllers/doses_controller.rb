8435e9d560facf5b9a10d49c2a955afeea754794
Chloe Baron <chloe.f.baron@gmail.com>
2018-05-17 17:01:15 -0400

    Need to correct delete icon

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,33 @@
 class DosesController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+
+    @dose.cocktail = @cocktail
+    @dose.save
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
 end
